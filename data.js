2a3a1aaeb0f6bde48bdee69c763b2ed1f46f056e
DemiusLs <dloss320@gmail.com>
2025-05-26 15:48:54 +0200

    feat: project init

---
+++ data.js
@@ -0,0 +1,172 @@
+plants = [
+  {
+    "nomeComune": "Camomilla",
+    "nomeScientifico": "Matricaria chamomilla",
+    "proprietaPrincipali": [
+      "Sedativa",
+      "Antinfiammatoria",
+      "Spasmolitica",
+      "Digestiva"
+    ],
+    "usiComuni": [
+      "Insonnia e disturbi del sonno",
+      "Ansia e nervosismo",
+      "Crampi mestruali e intestinali",
+      "Irritazioni cutanee e congiuntiviti"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/0"
+  },
+  {
+    "nomeComune": "Menta Piperita",
+    "nomeScientifico": "Mentha x piperita",
+    "proprietaPrincipali": [
+      "Digestiva",
+      "Antispasmodica",
+      "Carminativa",
+      "Rinfrescante"
+    ],
+    "usiComuni": [
+      "Nausea e vomito",
+      "Indigestione e flatulenza",
+      "Sindrome dell'intestino irritabile",
+      "Raffreddore e congestione nasale"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/33"
+  },
+  {
+    "nomeComune": "Aloe Vera",
+    "nomeScientifico": "Aloe barbadensis miller",
+    "proprietaPrincipali": [
+      "Cicatrizzante",
+      "Idratante",
+      "Antinfiammatoria",
+      "Lenitiva"
+    ],
+    "usiComuni": [
+      "Scottature solari e ustioni minori",
+      "Irritazioni e arrossamenti della pelle",
+      "Ferite e abrasioni",
+      "Stipsi (succo)"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/30"
+  },
+  {
+    "nomeComune": "Calendula",
+    "nomeScientifico": "Calendula officinalis",
+    "proprietaPrincipali": [
+      "Cicatrizzante",
+      "Antinfiammatoria",
+      "Lenitiva",
+      "Antisettica"
+    ],
+    "usiComuni": [
+      "Ferite, tagli e abrasioni",
+      "Irritazioni e infiammazioni cutanee",
+      "Pelle secca e screpolata",
+      "Dermatite da pannolino"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/29"
+  },
+  {
+    "nomeComune": "Lavanda",
+    "nomeScientifico": "Lavandula angustifolia",
+    "proprietaPrincipali": [
+      "Rilassante",
+      "Antispasmodica",
+      "Antisettica",
+      "Cicatrizzante"
+    ],
+    "usiComuni": [
+      "Ansia e stress",
+      "Insonnia",
+      "Mal di testa",
+      "Punture d'insetto e piccole ustioni"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/28"
+  },
+  {
+    "nomeComune": "Echinacea",
+    "nomeScientifico": "Echinacea purpurea",
+    "proprietaPrincipali": [
+      "Immunostimolante",
+      "Antivirale",
+      "Antibatterica",
+      "Antinfiammatoria"
+    ],
+    "usiComuni": [
+      "Prevenzione e trattamento raffreddore e influenza",
+      "Infezioni delle vie respiratorie",
+      "Supporto immunitario"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/31"
+  },
+  {
+    "nomeComune": "Tiglio",
+    "nomeScientifico": "Tilia cordata / platyphyllos",
+    "proprietaPrincipali": [
+      "Sedativa",
+      "Antispasmodica",
+      "Diaforetica (favorisce sudorazione)",
+      "Ansiolitica"
+    ],
+    "usiComuni": [
+      "Insonnia e nervosismo",
+      "Tosse e raffreddore",
+      "Tensione nervosa e palpitazioni"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/32"
+  },
+  {
+    "nomeComune": "Iperico",
+    "nomeScientifico": "Hypericum perforatum",
+    "proprietaPrincipali": [
+      "Antidepressivo (lieve-moderato)",
+      "Ansionlitico",
+      "Cicatrizzante",
+      "Antinfiammatorio"
+    ],
+    "usiComuni": [
+      "Stati depressivi lievi o moderati",
+      "Ansia",
+      "Insonnia (legata a stati ansiosi)",
+      "Ferite e ustioni (uso topico)"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/34"
+  },
+  {
+    "nomeComune": "Valeriana",
+    "nomeScientifico": "Valeriana officinalis",
+    "proprietaPrincipali": [
+      "Sedativa",
+      "Ipnotica",
+      "Ansiolitica",
+      "Spasmolitica"
+    ],
+    "usiComuni": [
+      "Insonnia",
+      "Ansia e nervosismo",
+      "Stati di agitazione",
+      "Crampi e spasmi"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/35"
+  },
+  {
+    "nomeComune": "Zenzero",
+    "nomeScientifico": "Zingiber officinale",
+    "proprietaPrincipali": [
+      "Antiemetico",
+      "Digestivo",
+      "Antinfiammatorio",
+      "Antiossidante"
+    ],
+    "usiComuni": [
+      "Nausea (mal d'auto, gravidanza, chemioterapia)",
+      "Indigestione",
+      "Dolori muscolari e articolari",
+      "Raffreddore e gola irritata"
+    ],
+    "immagineURL": "http://googleusercontent.com/image_generation_content/36"
+  }
+]
+
+export default plants; 
