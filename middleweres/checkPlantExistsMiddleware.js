3758d71201ab551d0db968b8089fb8847a297ec6
DemiusLs <dloss320@gmail.com>
2025-05-28 16:40:53 +0200

    feat: checkPLant done , plant controller to fix

---
+++ middleweres/checkPlantExistsMiddleware.js
@@ -0,0 +1,27 @@
+import plants from "../data/data.js";
+
+function checkPlantExistsMiddleware (req , res , event){
+
+    const plantId = req.params.id;
+
+    const index = plants.findIndex((plant) => plant.id === plantId);
+
+    // Se l'indice è -1 
+
+    if(index === -1){
+
+        // Ritorno errore e mi fermo
+        res.status(404);
+        res.json({
+            error : "Pianta non trovata"
+        })
+
+    }else{
+        // salvo l'indice come nuova chiave di req
+        req.plantIndex = index;
+    }
+
+    next();
+}
+
+export default checkPlantExistsMiddleware; 
--- routers/plants.js
+++ routers/plants.js
@@ -1,5 +1,6 @@
 import express from "express"
 import plantController from "../controllers/plantController.js";
+import checkPlantExistsMiddleware from "../middleweres/checkPlantExistsMiddleware.js";
 
 
 
@@ -7,27 +8,27 @@ const router = express.Router();
 
 //INDEX
 // lettura di tutti gli elementi 
-router.get("/", plantController.index )
+router.get("/", plantController.index)
 
 
 //SHOW 
 //lettura di un elemento singolo
-router.get("/:id", plantController.show  )
+router.get("/:id", checkPlantExistsMiddleware, plantController.show)
 
 
 //STORE
 //inserisco un nuovo elemento
-router.post('/', plantController.store )
+router.post('/', plantController.store)
 
 
 //UPDATE
 //modifico un elemento
-router.put("/:id", plantController.update )
+router.put("/:id", checkPlantExistsMiddleware, plantController.update)
 
 
 //DELETE
 //elimino un elemento
-router.delete("/:id",  plantController.destroy)
+router.delete("/:id", checkPlantExistsMiddleware, plantController.destroy)
 
 
 export default router;
